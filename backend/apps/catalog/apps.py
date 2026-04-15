74072d35fd93d6c7f96548d8019a997932110c0a
Макеева Олеся <makeeva.o.a@yandex.ru>
2026-04-15 05:49:07 +0300

    fix: явная загрузка admin.py через ready() в CatalogConfig

--- backend/apps/catalog/apps.py
+++ backend/apps/catalog/apps.py
@@ -5,3 +5,6 @@ class CatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.catalog'
     verbose_name = 'Каталог товаров'
+
+    def ready(self):
+        import apps.catalog.admin  # noqa: F401
