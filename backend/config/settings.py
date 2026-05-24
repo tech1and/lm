a28f1c7a237ccc33eaf8d5e3454e1eab07b560ac
Макеева Олеся <makeeva.o.a@yandex.ru>
2026-05-24 07:10:08 +0300

    sitemap.xml

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    #'PAGE_SIZE': 20,
     'MAX_PAGE_SIZE': 1000,  # Максимальный размер страницы для обычных запросов
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
