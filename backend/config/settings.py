7bf9c3e383a2a4a8eba9a7c7404c32fc566a7d05
Макеева Олеся <makeeva.o.a@yandex.ru>
2026-05-25 22:11:43 +0300

    sitemaps django8

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -125,7 +125,8 @@ CORS_ALLOW_CREDENTIALS = True
 
 # 🔥 Доверенные источники для CSRF
 CSRF_TRUSTED_ORIGINS = os.getenv(
-    'CSRF_TRUSTED_ORIGINS'
+    'CSRF_TRUSTED_ORIGINS',
+    'http://localhost:3000'
 ).split(',')
 
 # 🔥 Убедитесь, что кука доступна по HTTPS
